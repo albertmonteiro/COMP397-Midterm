21b4182ab585c9b8984c5052289fc7f6f2a73e94
albertmonteiro <albertmonteiro@live.com>
2016-03-04 17:16:18 -0500

    Added labels

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -18,7 +18,7 @@ var scenes;
             // This method gives two default dices showing "1"
             this._initialize();
             // add the ROLL button to the PLAY scene
-            this._roll = new objects.Button("Roll", config.Screen.CENTER_X + 30, config.Screen.CENTER_Y + 180);
+            this._roll = new objects.Button("Roll", config.Screen.CENTER_X + 15, config.Screen.CENTER_Y + 180);
             this.addChild(this._roll);
             // ROLL Button event listener
             this._roll.on("click", this._rollButtonClick, this);
@@ -30,6 +30,7 @@ var scenes;
         };
         // PRIVATE METHODS
         Play.prototype._initialize = function () {
+            // Display default dice images with "1"
             this._dice = new Array();
             for (var dice = 0; dice < 2; dice++) {
                 this._dice[dice] = new createjs.Bitmap(assets.getResult("One"));
@@ -40,6 +41,8 @@ var scenes;
         };
         // ROLL Button click event handler
         Play.prototype._rollButtonClick = function (event) {
+            this.removeChild(this._firstLabel);
+            this.removeChild(this._secondLabel);
             // Generating two random values between 1 and 6
             this._firstDice = Math.floor((Math.random() * 6) + 1);
             this._secondDice = Math.floor((Math.random() * 6) + 1);
@@ -47,79 +50,127 @@ var scenes;
             // Adding the first dice on the screen
             switch (this._firstDice) {
                 case 1:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("One"));
                     this._displayDice.x = 134;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._firstLabel = new objects.Label("1", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._firstLabel);
                     break;
                 case 2:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Two"));
                     this._displayDice.x = 134;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._firstLabel = new objects.Label("2", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._firstLabel);
                     break;
                 case 3:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Three"));
                     this._displayDice.x = 134;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._firstLabel = new objects.Label("3", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._firstLabel);
                     break;
                 case 4:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Four"));
                     this._displayDice.x = 134;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._firstLabel = new objects.Label("4", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._firstLabel);
                     break;
                 case 5:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Five"));
                     this._displayDice.x = 134;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._firstLabel = new objects.Label("5", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._firstLabel);
                     break;
                 case 6:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Six"));
                     this._displayDice.x = 134;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._firstLabel = new objects.Label("6", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._firstLabel);
                     break;
             }
             // Adding the second dice on the screen
             switch (this._secondDice) {
                 case 1:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("One"));
                     this._displayDice.x = 334;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._secondLabel = new objects.Label("1", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._secondLabel);
                     break;
                 case 2:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Two"));
                     this._displayDice.x = 334;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._secondLabel = new objects.Label("2", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._secondLabel);
                     break;
                 case 3:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Three"));
                     this._displayDice.x = 334;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._secondLabel = new objects.Label("3", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._secondLabel);
                     break;
                 case 4:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Four"));
                     this._displayDice.x = 334;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._secondLabel = new objects.Label("4", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._secondLabel);
                     break;
                 case 5:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Five"));
                     this._displayDice.x = 334;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._secondLabel = new objects.Label("5", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._secondLabel);
                     break;
                 case 6:
+                    // Display image
                     this._displayDice = new createjs.Bitmap(assets.getResult("Six"));
                     this._displayDice.x = 334;
                     this._displayDice.y = 130;
                     this.addChild(this._displayDice);
+                    // Display text
+                    this._secondLabel = new objects.Label("6", "60px Consolas", "#000000", config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                    this.addChild(this._secondLabel);
                     break;
             }
         };
--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -8,6 +8,8 @@ module scenes {
         private _roll: createjs.Bitmap;
         private _firstDice:number;
         private _secondDice:number;
+        private _firstLabel:objects.Label;
+        private _secondLabel:objects.Label;
         
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
@@ -24,7 +26,7 @@ module scenes {
             // add the ROLL button to the PLAY scene
             this._roll = new objects.Button(
                 "Roll",
-                config.Screen.CENTER_X + 30,
+                config.Screen.CENTER_X + 15,
                 config.Screen.CENTER_Y + 180);
             this.addChild(this._roll);
             
@@ -43,6 +45,7 @@ module scenes {
         // PRIVATE METHODS
         private _initialize():void
         {
+            // Display default dice images with "1"
             this._dice = new Array<createjs.Bitmap>();
             for (var dice:number = 0; dice < 2; dice++)
             {
@@ -55,6 +58,8 @@ module scenes {
         
         // ROLL Button click event handler
         private _rollButtonClick(event: createjs.MouseEvent) {
+            this.removeChild(this._firstLabel);
+            this.removeChild(this._secondLabel);
             // Generating two random values between 1 and 6
             this._firstDice = Math.floor((Math.random() * 6) + 1);
             this._secondDice = Math.floor((Math.random() * 6) + 1);
@@ -63,80 +68,152 @@ module scenes {
             // Adding the first dice on the screen
             switch (this._firstDice) {
                     case 1:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("One"));
                         this._displayDice.x = 134;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._firstLabel = new objects.Label(
+                        "1", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._firstLabel);
                         break;
                     case 2:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Two"));
                         this._displayDice.x = 134;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._firstLabel = new objects.Label(
+                        "2", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._firstLabel);
                         break;
                     case 3:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Three"));
                         this._displayDice.x = 134;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._firstLabel = new objects.Label(
+                        "3", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._firstLabel);
                         break;
                     case 4:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Four"));
                         this._displayDice.x = 134;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._firstLabel = new objects.Label(
+                        "4", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._firstLabel);
                         break;
                     case 5:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Five"));
                         this._displayDice.x = 134;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._firstLabel = new objects.Label(
+                        "5", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._firstLabel);
                         break;
                     case 6:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Six"));
                         this._displayDice.x = 134;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._firstLabel = new objects.Label(
+                        "6", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 50, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._firstLabel);
                         break;
                 }
                 
-            // Adding the second dice on the screen
+                // Adding the second dice on the screen
                 switch (this._secondDice) {
                     case 1:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("One"));
                         this._displayDice.x = 334;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._secondLabel = new objects.Label(
+                        "1", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._secondLabel);
                         break;
                     case 2:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Two"));
                         this._displayDice.x = 334;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._secondLabel = new objects.Label(
+                        "2", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._secondLabel);
                         break;
                     case 3:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Three"));
                         this._displayDice.x = 334;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._secondLabel = new objects.Label(
+                        "3", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._secondLabel);
                         break;
                     case 4:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Four"));
                         this._displayDice.x = 334;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._secondLabel = new objects.Label(
+                        "4", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._secondLabel);
                         break;
                     case 5:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Five"));
                         this._displayDice.x = 334;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._secondLabel = new objects.Label(
+                        "5", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._secondLabel);
                         break;
                     case 6:
+                        // Display image
                         this._displayDice = new createjs.Bitmap(assets.getResult("Six"));
                         this._displayDice.x = 334;
                         this._displayDice.y = 130;
                         this.addChild(this._displayDice);
+                        // Display text
+                        this._secondLabel = new objects.Label(
+                        "6", "60px Consolas", "#000000",
+                        config.Screen.CENTER_X = 134 + 250, config.Screen.CENTER_Y = 130 + 150);
+                        this.addChild(this._secondLabel);
                         break;
                 }
         }
